4326f30ceaa8448740eb2fb8d7aa92a954dd84bb
NausAlexus <naus.lexus@mail.ru>
2025-02-17 11:53:15 +0300

    fix

--- thanks_form.php
+++ thanks_form.php
@@ -2,11 +2,12 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $message = $_POST["message"];
 	$to = "support@" . $_SERVER['HTTP_HOST'];
     $subject = "New Contact Form Submission";
     $body = "Name: $name\n";
     $body .= "Email: $email\n";
-    $body .= "Tel:\n$tel";
+    $body .= "Message: \n$message";
     $headers = "From: $to \r\n";
     mail($to, $subject, $body, $headers);
     // Redirect to thank you page
